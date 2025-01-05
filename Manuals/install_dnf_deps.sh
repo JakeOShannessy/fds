6f6722c76ed9ccea77cfc8c13edf0922ad08c2bd
Jake O'Shannessy <joshannessy@threadway.com>
2025-01-05 21:22:19 +0000

    add some docs stuff

---
+++ Manuals/install_dnf_deps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo dnf install \
+    texlive-multirow \
+    texlive-pgfplots \
+    texlive-tocloft \
+    texlive-pict2e \
+    texlive-relsize \
+    texlive-threeparttable \
+    texlive-tocbibind \
+    texlive-lastpage \
+    texlive-upquote \
+    texlive-moreverb \
+    texlive-morefloats \
+    texlive-placeins \
+    texlive-needspace \
+    texlive-siunitx \
+    texlive-listings
